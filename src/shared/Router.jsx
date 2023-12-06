3a2bb6e8c986342549a141e63a12d59aba92ca32
Yeomhyewon <duagpdnjs1@naver.com>
2023-12-06 20:54:48 +0900

    add : FestivalRegistration router 연결 (#19)

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './Layout';
 import Home from 'pages/Home';
+import FestivalRegistration from 'pages/FestivalRegistration/FestivalRegistration';
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/addFestival" element={<FestivalRegistration />} />
           <Route path="*" element={<Navigate replace to="/" />} />
         </Route>
       </Routes>
