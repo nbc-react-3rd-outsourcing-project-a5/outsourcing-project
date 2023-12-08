79591352274b645a4c22b01c106e97ab3b2fe949
Yeomhyewon <duagpdnjs1@naver.com>
2023-12-08 17:32:08 +0900

    fix : authSlice __getUsers 주석처리 (#24)

--- src/redux/modules/authSlice.js
+++ src/redux/modules/authSlice.js
@@ -2,15 +2,15 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { db } from 'fb/firebase';
 import { doc, getDoc } from 'firebase/firestore';
 
-export const __getUsers = createAsyncThunk('getUsers', async (userId, thunkAPI) => {
-  try {
-    const docRef = doc(db, 'user', userId);
-    const docSnap = await getDoc(docRef);
-    return thunkAPI.fulfillWithValue(docSnap);
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error);
-  }
-});
+// export const __getUsers = createAsyncThunk('getUsers', async (userId, thunkAPI) => {
+//   try {
+//     const docRef = doc(db, 'user', userId);
+//     const docSnap = await getDoc(docRef);
+//     return thunkAPI.fulfillWithValue(docSnap);
+//   } catch (error) {
+//     return thunkAPI.rejectWithValue(error);
+//   }
+// });
 
 const authSlice = createSlice({
   name: 'auth',
